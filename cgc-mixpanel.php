4cab3f1f85f429266737d9f5bd4b78bbf6f39e0e
Pippin Williamson <pippin@pippinsplugins.com>
2013-04-17 16:12:26 -0500

    Added tracking for all the things

--- cgc-mixpanel.php
+++ cgc-mixpanel.php
@@ -13,19 +13,145 @@ function cgc_rcp_mixpanel_tracking( $payment_data, $user_id, $posted ) {
 	if( ! function_exists( 'wp_mixpanel' ) || ! function_exists( 'rcp_get_subscription_name' ) )
 		return;
 
-	if( $posted['txn_type'] == 'web_accept' || $posted['txn_type'] == 'subscr_signup' ) {
+	$user         = get_userdata( $user_id );
+	$subscription = rcp_get_subscription( $user_id );
+	$rcp_payments = new RCP_Payments;
+	$new_user     = $rcp_payments->last_payment_of_user( $user_id );
 
-		$user  = get_userdata( $user_id );
+	$person_props                 = array();
+	$person_props['first_name']   = $user->first_name;
+	$person_props['last_name']    = $user->last_name;
+	$person_props['user_login']   = $user->user_login;
+	$person_props['user_email']   = $user->user_email;
+	$person_props['subscription'] = $subscription;
+	$person_props['status']       = 'Active';
 
-		$props = array();
-		$props['user_login']   = $user->user_login;
-		$props['user_email']   = $user->user_email;
-		$props['subscription'] = rcp_get_subscription( $user_id );
-		$props['signup_date']  = $payment_data['date'];
+	wp_mixpanel()->track_person( $user_id, $person_props );
 
-		//wp_mixpanel()->track( 'RCP Signup', $props );
-		//wp_mixpanel()->track_person( $user_id, $props );
+	switch( $posted['txn_type'] ) :
+
+		// New subscription
+		case 'subscr_signup' :
+
+
+			$event_props                 = array();
+			$event_props['distinct_id']  = $user_id;
+			$event_props['subscription'] = $subscription;
+
+			$type = ! empty( $new_user ) ? 'Renewal' : 'New Signup';
+
+			wp_mixpanel()->track( $type, $event_props );
+
+			$person_props['recurring'] = 'Yes';
+			wp_mixpanel()->track_person( $user_id, $person_props );
+
+			break;
+
+		// Subscription payment
+		case 'subscr_payment' :
+
+			$trans_props = array(
+				'amount' => $payment_data['amount']
+			);
+			wp_mixpanel()->track_transaction( $user_id, $trans_props );
+
+			break;
+
+		case 'web_accept' :
+
+			if( strtolower( $posted['payment_status'] ) === 'completed' ) {
+
+				$event_props                 = array();
+				$event_props['distinct_id']  = $user_id;
+				$event_props['subscription'] = $subscription;
+
+				$type = ! empty( $new_user ) ? 'Renewal' : 'New Signup';
+
+				wp_mixpanel()->track( $type, $event_props );
+
+				$trans_props = array(
+					'amount' => $payment_data['amount']
+				);
+				wp_mixpanel()->track_transaction( $user_id, $trans_props );
+
+				$person_props['recurring'] = 'No';
+				wp_mixpanel()->track_person( $user_id, $person_props );
+
+			}
+
+			break;
+
+	endswitch;
+
+}
+add_action( 'rcp_valid_ipn', 'cgc_rcp_mixpanel_tracking', 10, 3 );
+
+
+function cgc_rcp_track_status_changes( $new_status, $user_id ) {
+
+	// We check for $_POST to make sure this only fires on the signup form
+	if( 'free' === $new_status && isset( $_POST['rcp_level'] ) ) {
+
+		$user                         = get_userdata( $user_id );
+
+		$person_props                 = array();
+		$person_props['first_name']   = $user->first_name;
+		$person_props['last_name']    = $user->last_name;
+		$person_props['user_login']   = $user->user_login;
+		$person_props['user_email']   = $user->user_email;
+		$person_props['subscription'] = rcp_get_subscription( $user_id );
+		$person_props['status']       = 'free';
+
+		wp_mixpanel()->track_person( $user_id, $person_props );
+
+		$event_props                 = array();
+		$event_props['distinct_id']  = $user_id;
+		$event_props['subscription'] = $subscription;
+
+		wp_mixpanel()->track( 'Free Signup', $event_props );
+
+	} else if( 'expired' === $new_status ) {
+
+		$user                         = get_userdata( $user_id );
+
+		$person_props                 = array();
+		$person_props['first_name']   = $user->first_name;
+		$person_props['last_name']    = $user->last_name;
+		$person_props['user_login']   = $user->user_login;
+		$person_props['user_email']   = $user->user_email;
+		$person_props['subscription'] = rcp_get_subscription( $user_id );
+		$person_props['status']       = 'Expired';
+		$person_props['recurring']    = 'No';
+
+		wp_mixpanel()->track_person( $user_id, $person_props );
+
+		$event_props                 = array();
+		$event_props['distinct_id']  = $user_id;
+		$event_props['subscription'] = $subscription;
+
+		wp_mixpanel()->track( 'Expired Membership', $event_props );
+
+	} elseif( 'cancelled' === $new_status ) {
+
+		$user                         = get_userdata( $user_id );
+
+		$person_props                 = array();
+		$person_props['first_name']   = $user->first_name;
+		$person_props['last_name']    = $user->last_name;
+		$person_props['user_login']   = $user->user_login;
+		$person_props['user_email']   = $user->user_email;
+		$person_props['subscription'] = rcp_get_subscription( $user_id );
+		$person_props['status']       = 'Cancelled';
+		$person_props['recurring']    = 'No';
+
+		wp_mixpanel()->track_person( $user_id, $person_props );
+
+		$event_props                 = array();
+		$event_props['distinct_id']  = $user_id;
+		$event_props['subscription'] = $subscription;
+
+		wp_mixpanel()->track( 'Cancelled Membership', $event_props );
 
 	}
 }
-add_action( 'rcp_valid_ipn', 'cgc_rcp_mixpanel_tracking', 10, 3 );
+add_action( 'rcp_set_status', 'cgc_rcp_track_free_signup', 10, 2 );
