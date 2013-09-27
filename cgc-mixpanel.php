92bf22607b74aaa2600180cd16657c2e2315519e
Pippin Williamson <pippin@pippinsplugins.com>
2013-09-27 15:19:25 -0500

    move track inside of submit

--- cgc-mixpanel.php
+++ cgc-mixpanel.php
@@ -30,9 +30,9 @@ mixpanel.init("018006ab8a267cc6d0a158dbfe41801a");
 jQuery(document).ready(function() {
 	jQuery('#rcp_submit').click(function( event ) {
 		mixpanel.alias( jQuery('#rcp_user_login').val() );
+		mixpanel.track( 'Page View: registration' );
 	});
 });
-mixpanel.track( 'Page View: registration' );
 <?php elseif( is_page( 'registration' ) ) : $user_data = get_userdata( get_current_user_id() ); ?>
 	mixpanel.identify( '<?php echo $user_data->user_login; ?>' );
 	mixpanel.track( 'Page View: registration' );
