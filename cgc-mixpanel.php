ee6b62f81c20178f4f03e75a82135f36e1837984
Pippin Williamson <pippin@pippinsplugins.com>
2013-09-27 15:12:11 -0500

    use submit

--- cgc-mixpanel.php
+++ cgc-mixpanel.php
@@ -28,7 +28,7 @@ mixpanel.init("018006ab8a267cc6d0a158dbfe41801a");
 <?php endif; ?>
 <?php if( is_page( 'registration' ) && ! is_user_logged_in() ) : ?>
 jQuery(document).ready(function() {
-	jQuery('body').on('submit', '#rcp_registration_form', function( event ) {
+	jQuery('#rcp_submit').click(function( event ) {
 		mixpanel.alias( jQuery('#rcp_user_login').val() );
 	});
 });
