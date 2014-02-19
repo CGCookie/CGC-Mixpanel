7067eaff02d821433217e3f52985dd9a75c95678
Pippin Williamson <pippin@pippinsplugins.com>
2014-02-19 14:49:21 -0600

    Setup ajax for JS identify

--- cgc-mixpanel.php
+++ cgc-mixpanel.php
@@ -17,6 +17,68 @@ if( ! class_exists( 'Mixpanel' ) ) {
 	require dirname( __FILE__ ) . '/mixpanel/lib/Mixpanel.php';
 }
 
+function cgc_mixpanel_js() {
+?>
+	<script type="text/javascript">
+
+	(function(e,b){if(!b.__SV){var a,f,i,g;window.mixpanel=b;a=e.createElement("script");a.type="text/javascript";a.async=!0;a.src=("https:"===e.location.protocol?"https:":"http:")+'//cdn.mxpnl.com/libs/mixpanel-2.2.min.js';f=e.getElementsByTagName("script")[0];f.parentNode.insertBefore(a,f);b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==
+	typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");for(g=0;g<i.length;g++)f(c,i[g]);
+	b._i.push([a,e,d])};b.__SV=1.2}})(document,window.mixpanel||[]);
+
+	mixpanel.init("<?php echo CGC_MIXPANEL_API; ?>");
+
+	var logged_in = cgc_get_query_vars()["logged-in"];
+
+	if( logged_in ) {
+		$.ajax({
+			type: "POST",
+			data: {
+				action: 'cgc_mixpanel_identify'
+			},
+			dataType: "json",
+			url: "<?php echo admin_url( 'admin-ajax.php' ); ?>",
+			success: function (response) {
+
+				mixpanel.alias( response.user_login );
+				mixpanel.identify( response.user_login );
+
+			}
+		}).fail(function (response) {
+
+			console.log( response );
+
+		}).done(function (response) {
+
+		});
+
+	}
+
+	function cgc_get_query_vars() {
+		var vars = [], hash;
+		var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+		for(var i = 0; i < hashes.length; i++) {
+			hash = hashes[i].split('=');
+			vars.push(hash[0]);
+			vars[hash[0]] = hash[1];
+		}
+		return vars;
+	}
+	</script>
+<?php
+}
+add_action( 'wp_head', 'cgc_mixpanel_js' );
+
+function cgc_mixpanel_ajax() {
+	if( is_user_logged_in() ) {
+		global $user_login;
+		get_currentuserinfo();
+		echo json_encode( array( 'user_login' => $user_login ) );
+		die();
+	}
+	die('-2');
+}
+add_action( 'wp_ajax_cgc_mixpanel_identify', 'cgc_mixpanel_ajax' );
+
 function cgc_mixpanel_user_login( $logged_in_cookie, $expire, $expiration, $user_id, $status = 'logged_in' ) {
 
 	$mp = Mixpanel::getInstance( CGC_MIXPANEL_API );
