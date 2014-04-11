f5c3feed1cac3378da2c909818ce6e3f8354fcba
Jonathan Williamson <jonathan@cgcookie.com>
2014-04-11 13:12:30 -0500

    Attempt to correctly reset download count upon membership payment

--- cgc-mixpanel.php
+++ cgc-mixpanel.php
@@ -326,7 +326,7 @@ function cgc_rcp_track_payment( $payment_id = 0, $args = array(), $amount ) {
 
 	// reset downloads for monthly members
 	if  ( $subscription == 'Monthly' || $subscription == 'Citizen Monthly' ){
-		$mp->people->set( $user->user_login, "Number of Downloads", 0 );
+		$mp->people->set( $user->user_login, "Number of Downloads" => 0 );
 	}
 	
 }
